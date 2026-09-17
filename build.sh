23820ad1c8c29815a95e992e2e63ea4319c070cc
c2942016420-dev <c2942016420@gmail.com>
2026-09-17 16:19:31 +0800

    Revise Germany and Athens itinerary

--- build.sh
+++ build.sh
@@ -3,4 +3,4 @@ set -euo pipefail
 
 mkdir -p public
 cat .source/part_*.b64 | base64 -d | gzip -dc > public/index.html
-printf '%s  %s\n' '9e3607b3e32a78e4bd38313a11c8785ccc5671371d3739d759cee846469588b4' 'public/index.html' | sha256sum -c -
+printf '%s  %s\n' '04776e192db786fea81ced27e9aaede62db268c4233a48fe82c4b41db50fe862' 'public/index.html' | sha256sum -c -
