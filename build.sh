2fcd4064bcaa11027a50329e28bb476bee4c4ac0
c2942016420-dev <c2942016420@gmail.com>
2026-09-17 22:19:18 +0800

    Revise Turkey itinerary and photo stops

--- build.sh
+++ build.sh
@@ -3,4 +3,4 @@ set -euo pipefail
 
 mkdir -p public
 cat .source/part_*.b64 | base64 -d | gzip -dc > public/index.html
-printf '%s  %s\n' '04776e192db786fea81ced27e9aaede62db268c4233a48fe82c4b41db50fe862' 'public/index.html' | sha256sum -c -
+printf '%s  %s\n' '42049121107f60959597d61ab1883d0ba40d971e6b998495dbd56114c652123c' 'public/index.html' | sha256sum -c -
